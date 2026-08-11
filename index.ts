94e50a28504aa1e40fad9565089a628bd0f4088d
Paolo <urrestipaolo@gmail.com>
2026-08-11 18:41:14 -0500

    feat: menú interactivo con ciclo y manejo de arreglo

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import readline from 'readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
-const rl = readline.createInterface({ input, output });
+ const rl = readline.createInterface({ input, output });
+
+
 // 🚫 No eliminar las líneas de arriba ⬆️
 
 // ✍️ Escribe tu código aquí 👇
@@ -16,10 +18,49 @@ console.log(systemName + "v" + version + " BIENVENIDO " + userName +
 // 🚫 No eliminar las líneas de abajo ⬇️
 rl.close();
 
-
-let control_de_flujos: string[];
+/// ------------------ ENTREGA MARTES --------------
+let resultado: string[] = []
 
 const answer = await rl.question("¿Cuál es tu nombre? ");
 console.log(`Hola, ${answer}!`);
 
-export {};
+
+let opciones: number = 0;
+
+while (opciones = 0){
+    console.log(`Bienvenido, usa alguna de las opciones:
+    - Agregar tarea: 1
+    - Eliminar ultima tarea: 2
+    - Lista tareas: 3
+    - Salir: 4`)
+    const answer: number = await rl.question();
+}
+
+switch(answer){
+    case 1:
+    const answer1 = await rl.question("¿Que desea agregar? ")
+    console.log(resultado.push(answer1));
+    break;
+
+    case 2:
+    const answer2 = await rl.question("¿Que desea eliminar? ")
+    console.log(resultado.pop + (answer2));
+    break;
+
+    case 3:
+    const answer3 = await rl.question("¿Que desea que se repita? ")
+        for(let i=1; i<=10; i++)
+    {
+    console.log(answer3)
+    }   
+    break;
+
+    case 4:
+    console.log("HASTA LUEGO");
+    break;
+
+}
+
+
+
+
